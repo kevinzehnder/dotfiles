0fa9cbef6248603e6de1c13406abadeec28a5c2c
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-03-20 07:10:03 +0100

    remove command completion because it crashes

--- .config/nvim/lua/c_completion.lua
+++ .config/nvim/lua/c_completion.lua
@@ -74,13 +74,13 @@ cmp.setup({
     }
   })
 
-  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
-  })
+  -- -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  -- cmp.setup.cmdline(':', {
+  --   sources = cmp.config.sources({
+  --     { name = 'path' }
+  --   }, {
+  --     { name = 'cmdline' }
+  --   })
+  -- })
 
 
