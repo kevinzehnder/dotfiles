07a50c4fcc997f6d6de38a3a508fc673a03e2d8a
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-03-20 08:02:53 +0100

    ctrl-e aborts completion

--- .config/nvim/lua/c_completion.lua
+++ .config/nvim/lua/c_completion.lua
@@ -15,8 +15,10 @@ cmp.setup({
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping.close(),
-
+    ['<C-e>'] = cmp.mapping({
+        i = cmp.mapping.abort(),
+        c = cmp.mapping.close(),
+      }), 
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         local entry = cmp.get_selected_entry()
