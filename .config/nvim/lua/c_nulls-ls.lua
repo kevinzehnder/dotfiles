c034ccff67b35f36fe28e473ef97161fc51c45cd
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-03-31 15:42:17 +0200

    add yamllint to null-ls

--- .config/nvim/lua/c_nulls-ls.lua
+++ .config/nvim/lua/c_nulls-ls.lua
@@ -1,6 +1,7 @@
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.black,
+        require("null-ls").builtins.diagnostics.yamllint,
 
         require("null-ls").builtins.diagnostics.pylint,
     },
