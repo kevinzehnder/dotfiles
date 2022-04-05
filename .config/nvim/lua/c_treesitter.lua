7eaf25d8ff67eda8426c8f27679aecddef3237ef
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-04-05 18:48:55 +0200

    disable treesitter indentation

--- .config/nvim/lua/c_treesitter.lua
+++ .config/nvim/lua/c_treesitter.lua
@@ -3,14 +3,14 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = "maintained", --only uses parsers that are maintained
   sync_install = false,
-  ignore_install = { "javascript" },
   highlight = { --enable highlighting
     enable = true,
+    use_languagetree = true,
     -- disable = { "c", "rust" },
     additional_vim_regex_highlighting = false,
   },
   indent = {
-    enable = true,
+    enable = false,
     disable = { "yaml" },
   }
 }
