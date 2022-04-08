2e089a7476873736a19c17670c84318513e19f0e
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-04-08 19:21:06 +0200

    cleanup file structure

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,16 +3,20 @@ require('plugins')
 
 -- plugin settings
 require('c_nvimtree')
-require('c_completion')
-require('c_nulls-ls')
-require('c_lsp')
-require('c_treesitter')
+ 
 require('c_fzf')
+require('c_telescope')
+ 
 require('c_lualine')
 require('c_gitsigns')
 require('c_trouble')
-require('c_telescope')
+ 
 require('c_toggleterm')
+
+require('c_treesitter')
+require('c_completion')
+require('c_nulls-ls')
+require('c_lsp')
 
 -- general vim settings
 require('vimoptions')
