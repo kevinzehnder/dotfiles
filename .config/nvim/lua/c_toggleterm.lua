dff7899c08e7dd0e8be981444640df1f4d58d4c5
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-04-09 07:20:13 +0200

    add toggleterm configuration module

---
+++ .config/nvim/lua/c_toggleterm.lua
@@ -0,0 +1,8 @@
+require('toggleterm').setup{
+
+  open_mapping = [[<C-A-j>]],
+  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  terminal_mappings = true, -- whether or not the open mapping applies in the opened termina
+
+
+}
