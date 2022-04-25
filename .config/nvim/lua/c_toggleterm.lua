74ca13f7629a1e19f23dbda46e2b9bb500de78ad
kevinzehnder <kevin.zehnder@protonmail.com>
2022-04-25 13:32:32 +0200

    change default terminal size to 30

--- .config/nvim/lua/c_toggleterm.lua
+++ .config/nvim/lua/c_toggleterm.lua
@@ -4,5 +4,7 @@ require('toggleterm').setup{
   insert_mappings = true, -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened termina
 
+  size = 30,
+
 
 }
