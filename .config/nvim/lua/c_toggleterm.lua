d5b5d2734d6598b73918436a3a877275c6e64520
kevinzehnder <kevin.zehnder@protonmail.com>
2022-06-27 16:47:36 +0200

    fix background issues with toggleterm

--- .config/nvim/lua/c_toggleterm.lua
+++ .config/nvim/lua/c_toggleterm.lua
@@ -5,6 +5,18 @@ require('toggleterm').setup{
   terminal_mappings = true, -- whether or not the open mapping applies in the opened termina
 
   size = 30,
-
+  highlights = {
+      -- highlights which map to a highlight group name and a table of it's values
+      -- NOTE: this is only a subset of values, any group placed here will be set for the terminal window split
+      Normal = {
+        -- guibg = "#afafaf",
+      },
+      NormalFloat = {
+        link = 'Normal'
+      },
+    },
+    shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+    shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+    start_in_insert = true,
 
 }
