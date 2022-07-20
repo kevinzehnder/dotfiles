5876754880d94dc69666e44d61e9ed74d6326b43
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-07-20 12:58:39 +0200

    add more Telescope bindings

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -33,19 +33,15 @@ nmap("<C-A-h>", ":NvimTreeToggle<CR>")
 
 -- FZF
 nmap("<Leader>p", ":Files<CR>")
--- nmap("<Leader>f", ":Files<CR>")
--- nmap("<C-f>", ":BLines<CR>")
--- nmap("<C-b>", ":Buffers<CR>")
--- nmap("<C-g>", ":RG<CR>")
 
 -- telescope
--- nmap("<C-p>", ":Telescope project_files<CR>")
 nmap("<C-p>", ":lua require('c_telescope').project_files()<CR>")
 nmap("<C-b>", ":Telescope buffers<CR>")
 nmap("<C-f>", ":Telescope current_buffer_fuzzy_find<CR>")
 nmap("<C-g>", ":Telescope live_grep<CR>")
 nmap("<Leader>T", ":Telescope <CR>")
 nmap("<Leader>m", ":Telescope marks<CR>")
+nmap("<Leader>d", ":Telescope treesitter<CR>")
 
 -- toggleterm
 -- nmap("<C-A-j>", ":ToggleTerm<CR>")
