626968e5bd7a0430e35dfd6ce4c5b73087d1e964
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-08-12 20:05:50 +0200

    change border of documentation hover windows

--- .config/nvim/lua/kz/lsp.lua
+++ .config/nvim/lua/kz/lsp.lua
@@ -88,3 +88,8 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+-- Change border of documentation hover window, See https://github.com/neovim/neovim/pull/13998.
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "rounded",
+})
