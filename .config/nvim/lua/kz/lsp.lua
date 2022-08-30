048bc0dc33b2f5c0320b11530732c3a2ac79398e
kevinzehnder <kevin.zehnder@protonmail.com>
2022-08-30 11:06:35 +0200

    reformat keymaps

--- .config/nvim/lua/kz/lsp.lua
+++ .config/nvim/lua/kz/lsp.lua
@@ -1,11 +1,12 @@
 -- lsp
 local lsp_installer = require("nvim-lsp-installer")
+local opts = { noremap = true, silent = true }
 
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+vim.api.nvim_set_keymap('n', '<Leader>e', ":lua vim.diagnostic.open_float()<CR>", opts)
+vim.api.nvim_set_keymap('n', '[d', ":lua vim.diagnostic.goto_prev()<CR>", opts)
+vim.api.nvim_set_keymap('n', ']d', ":lua vim.diagnostic.goto_next()<CR>", opts)
+vim.api.nvim_set_keymap('n', '<Leader>q', ":lua vim.diagnostic.setloclist()<CR>", opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -15,22 +16,24 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  --[[ -nvim_buf_set_keymap(buffer, mode, lhs, rhs, *opts) ]]
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', ":lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', ":lua vim.lsp.buf.definition()<CR>", opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
   vim.keymap.set('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() print(vim.lsp.buf.format{async=true})end, bufopts)
+  end, opts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>f', ":lua vim.lsp.buf.format({async=True})<CR>", opts)
+  --[[ vim.keymap.set('n', '<space>f', function() print(vim.lsp.buf.format { async = true }) end, opts) ]]
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -47,7 +50,7 @@ local enhance_server_opts = {
       yaml = {
         format = { enable = true },
         schemas = {
-          ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"]= "conf/**/*catalog*",
+          ["https://raw.githubusercontent.com/quantumblacklabs/kedro/develop/static/jsonschema/kedro-catalog-0.17.json"] = "conf/**/*catalog*",
           ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
           ["kubernetes"] = "/*.yml",
         },
@@ -93,4 +96,3 @@ end
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "rounded",
 })
-
