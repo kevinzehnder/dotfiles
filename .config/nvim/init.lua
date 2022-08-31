11247afd5b6de5b5a865a661f2a0e633d008e7aa
kevinzehnder <kevin.zehnder@protonmail.com>
2022-08-31 14:24:00 +0200

    update init lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,4 @@
--- plugins with packer
+-- plugins with packer 
 require('kz.plugins')
 
 -- general vim settings
@@ -6,26 +6,51 @@ require('kz.vimoptions')
 require('kz.colors')
 require('kz.keybindings')
 
--- -- plugin settings
+-- plugin settings
 require('kz.nvimtree')
 require('kz.autopairs')
 require('kz.commentary')
 require('kz.impatient')
- 
+
 require('kz.fzf')
 require('kz.telescope')
- 
+
 require('kz.lualine')
 require('kz.gitsigns')
 require('kz.trouble')
- 
-require('kz.toggleterm')
 
+require('kz.toggleterm')
 require('kz.treesitter')
 require('kz.completion')
+
 require('kz.null-ls')
 require('kz.lsp')
 
 require('kz.indentline')
 require('kz.dap')
 
+require('kz.project')
+
+
+-- require "user.options"
+-- require "user.keymaps"
+-- require "user.plugins"
+-- require "user.autocommands"
+-- require "user.colorscheme"
+-- require "user.cmp"
+-- require "user.telescope"
+-- require "user.treesitter"
+-- require "user.autopairs"
+-- require "user.comment"
+-- require "user.gitsigns"
+-- require "user.nvim-tree"
+-- require "user.bufferline"
+-- require "user.lualine"
+-- require "user.toggleterm"
+-- require "user.project"
+-- require "user.impatient"
+-- require "user.illuminate"
+-- require "user.indentline"
+-- require "user.alpha"
+-- require "user.lsp"
+-- require "user.dap"
