f07190c30cf9030932302d04c91664bc04d693e7
Kevin Zehnder <kevin.zehnder@protonmail.com>
2022-09-14 20:00:15 +0200

    add alpha configuration

--- .config/nvim/lua/kz/keybindings.lua
+++ .config/nvim/lua/kz/keybindings.lua
@@ -120,6 +120,7 @@ nmap("<Leader>x", ":q!<cr>")
 nmap("<Leader>w", ":w<cr>")
 nmap("<Leader>q", ":q<cr>")
 nmap("<Leader>vs", ":silent !code %:p<cr>") -- open current file in vscode
+nmap("<F2>", ":Alpha<CR>")
 
 -- Custom Shortcuts
 imap("jk", "<Esc>")
