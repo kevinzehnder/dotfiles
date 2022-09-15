94fa145b991b890d9b20aa3aef2e294d5319a057
kevinzehnder <kevin.zehnder@protonmail.com>
2022-09-15 08:03:47 +0200

    remove comments

--- .config/nvim/lua/kz/telescope.lua
+++ .config/nvim/lua/kz/telescope.lua
@@ -35,13 +35,6 @@ require('telescope').setup{
         find_command = { "rg", "--files", "--ignore"}
 
     }
-    -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
   },
   extensions = {
     fzf = {
@@ -51,11 +44,6 @@ require('telescope').setup{
       case_mode = "smart_case",
 
     }
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
   }
 }
 
