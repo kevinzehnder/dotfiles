475539a2c0cf69d67c354db5a281940868f3bd25
kevinzehnder <kevin.zehnder@protonmail.com>
2022-09-15 08:03:34 +0200

    move plugins around

--- .config/nvim/lua/kz/init.lua
+++ .config/nvim/lua/kz/init.lua
@@ -11,33 +11,26 @@ require('kz.keymap')
 require('kz.keybindings')
 
 -- plugin settings
-require('kz.nvimtree')
 require('kz.autopairs')
 require('kz.commentary')
 require('kz.impatient')
 
-require("harpoon").setup({
-    menu = {
-        width = vim.api.nvim_win_get_width(0) - 4,
-    }
-})
-
+require('kz.nvimtree')
 require('kz.fzf')
 require('kz.telescope')
+require('kz.harpoon')
+require('kz.project')
 
 require('kz.lualine')
+require('kz.indentline')
 require('kz.gitsigns')
-require('kz.trouble')
-
 require('kz.toggleterm')
+
+require('kz.trouble')
 require('kz.treesitter')
 require('kz.completion')
-
 require('kz.null-ls')
 require('kz.lsp')
 
-require('kz.indentline')
 require('kz.dap')
-
-require('kz.project')
 
