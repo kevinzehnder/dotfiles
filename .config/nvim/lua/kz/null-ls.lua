e791398ec87a400f88fda19f7f8b17e1da0b3194
kevinzehnder <kevin.zehnder@protonmail.com>
2022-11-28 05:47:53 +0100

    enable pylint and mypy

--- .config/nvim/lua/kz/null-ls.lua
+++ .config/nvim/lua/kz/null-ls.lua
@@ -3,7 +3,8 @@ require("null-ls").setup({
         -- python
         require("null-ls").builtins.formatting.isort,
         require("null-ls").builtins.formatting.black.with({ extra_args = { "--fast"}}),
-        -- require("null-ls").builtins.diagnostics.pylint,
+        require("null-ls").builtins.diagnostics.pylint,
+        require("null-ls").builtins.diagnostics.mypy,
 
         -- go
         require("null-ls").builtins.formatting.gofmt,
