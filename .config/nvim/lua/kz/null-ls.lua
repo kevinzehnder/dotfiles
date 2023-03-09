a8225fbbc7568b5c64d88d259766da18f241fcf6
kevinzehnder <kevin.zehnder@protonmail.com>
2023-03-09 10:48:32 +0100

    cleanup null_ls setup

--- .config/nvim/lua/kz/null-ls.lua
+++ .config/nvim/lua/kz/null-ls.lua
@@ -1,25 +1,33 @@
-require("null-ls").setup({
+null_ls = require("null-ls")
+null_ls.setup({
     sources = {
         -- python
-        require("null-ls").builtins.formatting.isort,
-        require("null-ls").builtins.formatting.black.with({ extra_args = { "--fast"}}),
-        require("null-ls").builtins.diagnostics.pylint,
-        require("null-ls").builtins.diagnostics.mypy,
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black.with({ extra_args = { "--fast"}}),
+        null_ls.builtins.diagnostics.pylint.with({
+              diagnostics_postprocess = function(diagnostic)
+                diagnostic.code = diagnostic.message_id
+              end,
+            }),
+        null_ls.builtins.diagnostics.mypy,
 
         -- go
-        require("null-ls").builtins.formatting.gofmt,
-        require("null-ls").builtins.formatting.goimports,
-        require("null-ls").builtins.formatting.golines,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.golines,
         
         -- yaml
-        require("null-ls").builtins.diagnostics.yamllint,
+        null_ls.builtins.diagnostics.yamllint,
 
         -- javascript
-        require("null-ls").builtins.formatting.prettier,
-        require("null-ls").builtins.formatting.eslint,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.eslint,
 
         -- rust
-        require("null-ls").builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.rustfmt,
     },
+
+
+
 })
 
