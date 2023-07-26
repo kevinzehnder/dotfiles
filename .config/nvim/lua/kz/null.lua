bafcecea64bbd23687ccad6593ce8f36f1dccb70
Kevin Zehnder <kevin.zehnder@protonmail.com>
2023-07-26 09:35:15 +0200

    add prettierd to null-ls

--- .config/nvim/lua/kz/null.lua
+++ .config/nvim/lua/kz/null.lua
@@ -10,6 +10,12 @@ null_ls.setup({
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.ruff,
 
+        null_ls.builtins.formatting.prettierd.with({
+            filetypes = {
+                "json", "yaml", "markdown", "graphql", "md", "txt",
+            },
+        }),
+
         -- go
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
