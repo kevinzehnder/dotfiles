6c4f8aa7405729ba5e3f49fd199a33f37f916015
Kevin Zehnder <kevin.zehnder@protonmail.com>
2023-07-28 09:34:04 +0200

    use better glyphs

--- .config/nvim/lua/kz/lsp.lua
+++ .config/nvim/lua/kz/lsp.lua
@@ -59,7 +59,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 -- appearance
-local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
