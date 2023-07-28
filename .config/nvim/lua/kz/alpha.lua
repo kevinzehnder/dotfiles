6963bcdb629eca6cda41a11a0b13abe0de7911b6
Kevin Zehnder <kevin.zehnder@protonmail.com>
2023-07-28 09:40:12 +0200

    change alpha glyphs

--- .config/nvim/lua/kz/alpha.lua
+++ .config/nvim/lua/kz/alpha.lua
@@ -13,13 +13,13 @@ dashboard.section.header.val = {
     [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-    dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+    dashboard.button("f", " " .. " Find file", ":lua require('kz.telescope').project_files()<CR>"),
     dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
     dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
     dashboard.button("c", " " .. " Config", ":lua require('kz.telescope').search_dotfiles()<CR>"),
-    dashboard.button("m", " " .. " Harpoon", ":lua require('harpoon.ui').toggle_quick_menu()<CR>"),
+    dashboard.button("m", "󱡅 " .. " Harpoon", ":lua require('harpoon.ui').toggle_quick_menu()<CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
