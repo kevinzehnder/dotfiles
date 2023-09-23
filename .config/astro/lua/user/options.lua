84fff79e0523fa457d8cf0a894f149526db211e0
kevinzehnder <kevin.zehnder@protonmail.com>
2023-09-23 07:31:26 +0200

    fix lazyvim scroll

--- .config/astro/lua/user/options.lua
+++ .config/astro/lua/user/options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    sidescrolloff = 0,
 
     cmdheight = 1,
 
