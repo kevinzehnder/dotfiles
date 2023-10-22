ec67ed166f80ed9333ee12a912fbabdb66c61f6b
Kevin Zehnder <kevin.zehnder@protonmail.com>
2023-10-22 15:08:58 +0200

    fix config for new version changes

--- .config/astro/lua/user/plugins/indentline.lua
+++ .config/astro/lua/user/plugins/indentline.lua
@@ -1,17 +1,7 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   opts = {
-
-    -- char = "▏",
-    show_trailing_blankline_indent = true,
-    show_first_indent_level = true,
-    use_treesitter = true,
-    show_current_context = true,
-    buftype_exclude = { "terminal", "nofile" },
-    filetype_exclude = {
-      "help",
-      "packer",
-      "NvimTree",
-    },
+    indent = { char = "▎", tab_char = "▎" },
   },
 }
