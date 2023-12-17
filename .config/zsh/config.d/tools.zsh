10163236e4207a4da45aad1f94881521010f6850
Kevin Zehnder <kevin.zehnder@protonmail.com>
2023-12-17 11:53:46 +0100

    add navi

--- .config/zsh/config.d/tools.zsh
+++ .config/zsh/config.d/tools.zsh
@@ -3,4 +3,5 @@ zi wait lucid light-mode as"program" from"gh-r" for \
     joehillen/sysz \
     ver"v0.7.5" pick"b/x86*/release/sd" chmln/sd \
     pick"btop/bin/btop" aristocratos/btop \
+    denisidoro/navi \
 
