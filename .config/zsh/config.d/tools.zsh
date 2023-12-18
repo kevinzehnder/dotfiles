493f18cc46ca6d65b6cf9d5604a4aaafb60caa3b
kevinzehnder <kevin.zehnder@protonmail.com>
2023-12-18 08:24:00 +0100

    remove sysz

--- .config/zsh/config.d/tools.zsh
+++ .config/zsh/config.d/tools.zsh
@@ -1,6 +1,5 @@
 zi wait lucid light-mode as"program" from"gh-r" for \
     jesseduffield/lazydocker \
-    joehillen/sysz \
     ver"v0.7.5" pick"b/x86*/release/sd" chmln/sd \
     pick"btop/bin/btop" aristocratos/btop \
     denisidoro/navi \
