0ccd34f787b2df5ca48d8834bd8427736cb375b5
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-03-02 19:33:45 +0100

    add kustomize

--- .config/zsh/config.d/tools.zsh
+++ .config/zsh/config.d/tools.zsh
@@ -4,4 +4,5 @@ zi wait lucid light-mode as"program" from"gh-r" for \
     pick"btop/bin/btop" aristocratos/btop \
     denisidoro/navi \
     pick"bin/linux_amd64/kubelogin" Azure/kubelogin \
+    ver"kustomize/v5.3.0" kubernetes-sigs/kustomize \
 
