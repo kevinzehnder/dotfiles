688aca36eff97ba552a6e5dbc2082c6e4e2c6950
kevinzehnder <kevin.zehnder@protonmail.com>
2024-03-15 07:47:40 +0100

    move global taskfile

--- .config/zsh/config.d/tasks.zsh
+++ .config/zsh/config.d/tasks.zsh
@@ -1,3 +1,21 @@
+function tg() {
+  # Capture the output of the pipeline into a variable
+  local selected_tasks=$(task --taskfile ${HOME}/.Taskfile.yaml --list \
+    | cut -d ' ' -f2 \
+    | tail -n +2 \
+    | sed 's/://' \
+    | sort \
+    | fzf -m --reverse --preview 'task ${HOME}/.Taskfile.yaml --summary {}')
+
+  # Check if any tasks were selected
+  if [ -n "$selected_tasks" ]; then
+    # Loop through the selected tasks and execute the command with each task as an argument
+    for task in $selected_tasks; do
+      task "$@" $task
+    done
+  fi
+}
+
 function t() {
   # Capture the output of the pipeline into a variable
   local selected_tasks=$(task --list \
