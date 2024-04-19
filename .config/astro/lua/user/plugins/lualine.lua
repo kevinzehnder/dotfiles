32fb7a194ab749a5859adac61c1a122ae689eddb
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-04-19 15:47:21 +0200

    set lualine to not focus on dap UI

--- .config/astro/lua/user/plugins/lualine.lua
+++ .config/astro/lua/user/plugins/lualine.lua
@@ -55,6 +55,14 @@ return {
         "dapui_stacks",
         "dap-repl",
       },
+      ignore_focus = {
+        "dapui_watches",
+        "dapui_breakpoints",
+        "dapui_scopes",
+        "dapui_console",
+        "dapui_stacks",
+        "dap-repl",
+      },
       globalstatus = true,
     }
 
