a6e59963ee041d808e093cbf960ee6542cdc1b39
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-04-19 15:47:11 +0200

    add dap config

---
+++ .config/astro/lua/user/plugins/dap.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = { "nvim-dap" },
+    cmd = { "DapInstall", "DapUninstall" },
+    opts = {
+      handlers = {},
+      ensure_installed = { "python", "delve" },
+    },
+  },
+}
