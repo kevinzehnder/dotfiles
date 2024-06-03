0e23853c5ee5c36be004726c2b0804f8d209ef60
kevinzehnder <kevin.zehnder@protonmail.com>
2024-06-03 10:00:09 +0200

    bump versions

--- .config/zsh/config.d/tools.zsh
+++ .config/zsh/config.d/tools.zsh
@@ -1,9 +1,9 @@
 zi wait lucid light-mode as"program" from"gh-r" for \
     jesseduffield/lazydocker \
-    ver"v0.7.5" pick"b/x86*/release/sd" chmln/sd \
+    pick"b/x86*/release/sd" chmln/sd \
     pick"btop/bin/btop" aristocratos/btop \
     denisidoro/navi \
     pick"bin/linux_amd64/kubelogin" Azure/kubelogin \
     ver"kustomize/v5.3.0" kubernetes-sigs/kustomize \
-    ver"0.1.13" pick"uv*/uv" @astral-sh/uv \
+    pick"uv*/uv" @astral-sh/uv \
     
