a7b00500ccfef759f37015605f5e0dba4c3c3d76
kevinzehnder <kevin.zehnder@protonmail.com>
2024-07-02 09:40:43 +0200

    remove gojq from aks function

--- .config/zsh/config.d/azcli.zsh
+++ .config/zsh/config.d/azcli.zsh
@@ -1,7 +1,7 @@
 function aks() {
   if [[ -n "$AKS_CLUSTER" && -n "$AKS_RG" ]]; then
      # Customize the appearance of the segment
-     az aks "$@" --name $AKS_CLUSTER --resource-group $AKS_RG | gojq
+     az aks "$@" --name $AKS_CLUSTER --resource-group $AKS_RG
  else
      echo "Error: Configure ENV variables AKS_CLUSTER and AKS_RG"
  fi
