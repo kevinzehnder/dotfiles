94e401b49560e7961c4c1c0067b4ebe1100a794b
kevinzehnder <kevin.zehnder@protonmail.com>
2024-09-09 08:39:46 +0200

    add sd as default tool

--- .config/zsh/config.d/tools.zsh
+++ .config/zsh/config.d/tools.zsh
@@ -3,5 +3,5 @@ zi wait lucid light-mode as"program" from"gh-r" for \
     denisidoro/navi \
     pick"bin/linux_amd64/kubelogin" Azure/kubelogin \
     ver"kustomize/v5.3.0" kubernetes-sigs/kustomize \
-    pick"ya*/yazi" sxyazi/yazi
-    # pick"sd*/sd" chmln/sd \
+    pick"ya*/yazi" sxyazi/yazi \
+    pick"sd*/sd" chmln/sd
