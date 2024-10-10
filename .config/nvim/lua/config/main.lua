c126562e3c0c9f1a3c54b792d02178fec277272d
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-10-10 05:12:06 +0200

    add auto-toggle nvim-tree

--- .config/nvim/lua/config/main.lua
+++ .config/nvim/lua/config/main.lua
@@ -159,3 +159,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- auto toggle nvim-tree
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
+	callback = function(args)
+		if vim.fn.expand("%:p") ~= "" then
+			vim.api.nvim_del_autocmd(args.id)
+			vim.cmd("noautocmd NvimTreeOpen")
+			vim.schedule(function()
+				vim.cmd("wincmd p")
+			end)
+		end
+	end,
+})
