69de568705db16dac4e8784d683efed02103aecc
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-10-12 02:50:29 +0200

    disable auto theme in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,6 @@ require("config.lazy")
 require("config.colors")
 
 if vim.g.neovide then
-	vim.g.neovide_theme = "auto"
+	-- vim.g.neovide_theme = "auto"
 	vim.o.guifont = "IosevkaTerm_Nerd_Font:h12" -- text below applies for VimScript
 end
