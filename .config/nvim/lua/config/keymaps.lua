87bdce372303e2cd2179f520e9a9d9f8fa6dda29
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-10-14 23:48:01 +0200

    remove diffview keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -79,15 +79,3 @@ vim.keymap.set("n", "<A-left>", ":vertical resize +2<CR>")
 -- insert mode mappings
 vim.keymap.set("i", "<C-l>", "<Right>")
 vim.keymap.set("i", "<C-k>", "<Up>")
-
--- diffview remaps
-local actions = require("diffview.actions")
-vim.keymap.set("n", "<leader>oo", actions.conflict_choose("ours"), { desc = "Choose the OURS version of a conflict" })
-vim.keymap.set("n", "<leader>ot", actions.conflict_choose("theirs"), { desc = "Choose the THEIRS version of a conflict" })
-vim.keymap.set("n", "<leader>ob", actions.conflict_choose("base"), { desc = "Choose the BASE version of a conflict" })
-vim.keymap.set("n", "<leader>oa", actions.conflict_choose("all"), { desc = "Choose all the versions of a conflict" })
-vim.keymap.set("n", "ox", actions.conflict_choose("none"), { desc = "Delete the conflict region" })
-vim.keymap.set("n", "<leader>oO", actions.conflict_choose_all("ours"), { desc = "Choose the OURS version of a conflict for the whole file" })
-vim.keymap.set("n", "<leader>oT", actions.conflict_choose_all("theirs"), { desc = "Choose the THEIRS version of a conflict for the whole file" })
-vim.keymap.set("n", "<leader>oB", actions.conflict_choose_all("base"), { desc = "Choose the BASE version of a conflict for the whole file" })
-vim.keymap.set("n", "<leader>oA", actions.conflict_choose_all("all"), { desc = "Choose all the versions of a conflict for the whole file" })
