16399438b4c6ce0dafa69161d61b8372a0b2b1d1
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-10-15 14:37:29 +0200

    remove lazy plugins from vscode setup

--- .config/nvim_vscode/init.lua
+++ .config/nvim_vscode/init.lua
@@ -1,38 +1,6 @@
 -- Print a message to confirm this config is being loaded
-print("Loading nvim_vscode configuration")
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Set space as the leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Plugin setup
-require("lazy").setup({
-  {
-    "kylechui/nvim-surround",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
-    end
-  },
-  -- ... other plugins ...
-})
 
 -- Set up keybindings
 -- VSCode-specific keybindings
@@ -40,64 +8,63 @@ local vscode = require('vscode')
 
 -- Map leader+w to save in VSCode
 vim.keymap.set('n', '<leader>w', function()
-vscode.call('workbench.action.files.save')
-print("workbench.action.files.save")
+	vscode.call('workbench.action.files.save')
+	print("workbench.action.files.save")
 end, { noremap = true, silent = true, desc = "Save file" })
 
 -- Map leader+c to close buffer in VSCode
 vim.keymap.set('n', '<leader>c', function()
-vscode.call('workbench.action.closeActiveEditor')
+	vscode.call('workbench.action.closeActiveEditor')
 end, { noremap = true, silent = true, desc = "Close current buffer" })
 
 -- Customize scrolling
 vim.keymap.set('n', '<C-e>', function()
-vscode.call('scrollLineDown')
+	vscode.call('scrollLineDown')
 end, { noremap = true, silent = true, desc = "Scroll down" })
 
 vim.keymap.set('n', '<C-z>', function()
-vscode.call('scrollLineUp')
+	vscode.call('scrollLineUp')
 end, { noremap = true, silent = true, desc = "Scroll up" })
 
 -- Also add these for visual and operator-pending modes
-vim.keymap.set({'v', 'x'}, '<C-e>', function()
-vscode.call('scrollLineDown')
+vim.keymap.set({ 'v', 'x' }, '<C-e>', function()
+	vscode.call('scrollLineDown')
 end, { noremap = true, silent = true, desc = "Scroll down" })
 
-vim.keymap.set({'v', 'x'}, '<C-z>', function()
-vscode.call('scrollLineUp')
+vim.keymap.set({ 'v', 'x' }, '<C-z>', function()
+	vscode.call('scrollLineUp')
 end, { noremap = true, silent = true, desc = "Scroll up" })
 
 -- Stage selection
 vim.keymap.set('v', '<leader>gs', function()
-vscode.call('git.stageSelectedRanges')
+	vscode.call('git.stageSelectedRanges')
 end, { noremap = true, silent = true, desc = "Git Stage Selection" })
 
 -- Unstage selection
 vim.keymap.set('v', '<leader>gu', function()
-vscode.call('git.unstageSelectedRanges')
+	vscode.call('git.unstageSelectedRanges')
 end, { noremap = true, silent = true, desc = "Git Unstage Selection" })
 
 -- Stage current file
 vim.keymap.set('n', '<leader>gf', function()
-vscode.call('git.stage')
+	vscode.call('git.stage')
 end, { noremap = true, silent = true, desc = "Git Stage File" })
 
 -- Unstage current file
 vim.keymap.set('n', '<leader>gF', function()
-vscode.call('git.unstage')
+	vscode.call('git.unstage')
 end, { noremap = true, silent = true, desc = "Git Unstage File" })
 
 -- Show Git changes in file
 vim.keymap.set('n', '<leader>gd', function()
-vscode.call('git.openChange')
+	vscode.call('git.openChange')
 end, { noremap = true, silent = true, desc = "Show Git Changes" })
 
 -- Revert current changes
 vim.keymap.set('n', '<leader>gr', function()
-vscode.call('git.revertSelectedRanges')
+	vscode.call('git.revertSelectedRanges')
 end, { noremap = true, silent = true, desc = "Revert Selection" })
 
 
 -- Common settings (will apply both in VSCode and regular Neovim)
-vim.opt.clipboard = 'unnamedplus'  -- Use system clipboard
-
+vim.opt.clipboard = 'unnamedplus' -- Use system clipboard
