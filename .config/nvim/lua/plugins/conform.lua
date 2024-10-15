26e6c4cfd29b0e416615ee0eeb90d62e10ea3869
Kevin Zehnder <kevin.zehnder@protonmail.com>
2024-10-15 15:54:20 +0200

    remove python config from conform and stick to ruff_lsp

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -20,7 +20,6 @@ return {
 		-- Define your formatters
 		formatters_by_ft = {
 			-- lua = { "stylua" },
-			python = { "isort", "black" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 		},
 		-- Set default options
