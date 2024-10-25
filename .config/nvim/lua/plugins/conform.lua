ac4eb76f3ff980f9c58f73cf45b893fe3252231d
kevinzehnder <kevin.zehnder@protonmail.com>
2024-10-25 07:05:29 +0200

    add yamlfmt to conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -21,6 +21,7 @@ return {
 		formatters_by_ft = {
 			-- lua = { "stylua" },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
+			yaml = { "yamlfmt", stop_after_first = true },
 		},
 		-- Set default options
 		default_format_opts = {
