6dc01fe9e3dd2433519e856ee2fc87bf4417a0e7
kevinzehnder <kevin.zehnder@protonmail.com>
2025-01-07 11:03:37 +0100

    improve procs

--- .config/zsh/config.d/procs.zsh
+++ .config/zsh/config.d/procs.zsh
@@ -1,7 +1,21 @@
-function procs_ports() {
-    procs --color always --load-config ~/.config/procs/procs_ports.toml "$@" | fzf --ansi
+function procsp() {
+	sudo -v
+    sudo procs --color always --load-config ~/.config/procs/procs_ports.toml "$@" | fzf --ansi
 }
 
-function procs_resources() {
-    procs --color always --load-config ~/.config/procs/procs_resources.toml "$@" | fzf --ansi
+function procsl() {                                                                                                                                                                  ☸  fw-awf-prd-euw-003-aks
+        sudo -v
+        sudo procs --use-config=large
+}
+
+function psk() {
+   sudo -v
+   sudo procs | fzf --ansi \
+       --preview "sudo procs --pager=disable --tree {1}" \
+       --preview-window=down \
+       --bind='ctrl-r:reload(sudo procs)' \
+       --header='[CTRL-R] reload [ENTER] kill' \
+       --height=100% \
+       --layout=reverse \
+       | awk '{print $1}' | xargs -r sudo kill -9
 }
