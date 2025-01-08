5564ce5f7abf1d183860a6d9a666669e31407a6e
kevinzehnder <kevin.zehnder@protonmail.com>
2025-01-08 15:46:10 +0100

    add custom per host definitions

---
+++ .config/zsh/config.d/custom.zsh
@@ -0,0 +1,14 @@
+if [[ $(hostname) == "towerKZ" ]]; then
+    zi wait lucid light-mode as"program" from"gh-r" for \
+        mv"gojq* ->gojq" pick"gojq/gojq" itchyny/gojq \
+        go-task/task \
+        derailed/k9s \
+        jesseduffield/lazydocker \
+        zellij-org/zellij \
+        twpayne/chezmoi \
+        pick"btop/bin/btop" aristocratos/btop \
+        pick"bin/linux_amd64/kubelogin" Azure/kubelogin \
+        ver"kustomize/v5.3.0" kubernetes-sigs/kustomize \
+        pick"ya*/yazi" sxyazi/yazi \
+        pick"sd*/sd" chmln/sd
+fi
