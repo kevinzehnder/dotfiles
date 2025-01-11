04cd28fe578b0a0551a7150ee83043ae415b2083
kevinzehnder <kevin.zehnder@protonmail.com>
2025-01-11 09:12:45 +0100

    fix paging

--- .config/zsh/config.d/system.zsh
+++ .config/zsh/config.d/system.zsh
@@ -54,7 +54,7 @@ function units() {
            --preview-window=right:60%:wrap \
            --header $'System Units | CTRL-R: reload\nCTRL-L: journal | CTRL-S: start | CTRL-D: stop | CTRL-T: restart' \
            --bind "ctrl-r:reload($cmd | awk '{print \$1}' | rg '\.service')" \
-           --bind "ctrl-l:execute(sudo journalctl -n 2000 -u {1} --no-pager | bat --paging=always -l syslog --style=numbers)" \
+           --bind "ctrl-l:execute(sudo journalctl -n 2000 -u {1} --no-pager | bat -l syslog -p --pager='less -R +G')" \
            --bind "ctrl-s:execute(sudo systemctl start {1})" \
            --bind "ctrl-d:execute(sudo systemctl stop {1})" \
            --bind "ctrl-t:execute(sudo systemctl restart {1})"
@@ -70,7 +70,7 @@ function timers() {
            --preview-window=right:60%:wrap \
            --header $'System Timers | CTRL-R: reload\nCTRL-L: journal | CTRL-S: start | CTRL-D: stop | CTRL-T: restart' \
            --bind "ctrl-r:reload(systemctl list-timers --all --no-pager | tail -n +2 | head -n -5 | awk '{print \$(NF-1)}')" \
-           --bind "ctrl-l:execute(sudo journalctl -n 2000 -u {} --no-pager | bat --paging=always -l=syslog --style=numbers )" \
+           --bind "ctrl-l:execute(sudo journalctl -n 2000 -u {} --no-pager | bat -l syslog -p --pager='less -R +G')" \
            --bind "ctrl-s:execute(sudo systemctl start {})" \
            --bind "ctrl-d:execute(sudo systemctl stop {})" \
            --bind "ctrl-t:execute(sudo systemctl restart {})"
