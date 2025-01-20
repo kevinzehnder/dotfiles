5ddd7b5e87f6eda534028cee6daecf07597afaa9
kevinzehnder <kevin.zehnder@protonmail.com>
2025-01-20 10:16:47 +0100

    add jnv

--- .config/zsh/config.d/custom.zsh
+++ .config/zsh/config.d/custom.zsh
@@ -10,5 +10,6 @@ if [[ $(hostname) == "towerKZ" ]]; then
         pick"bin/linux_amd64/kubelogin" Azure/kubelogin \
         ver"kustomize/v5.3.0" kubernetes-sigs/kustomize \
         pick"ya*/yazi" sxyazi/yazi \
-        pick"sd*/sd" chmln/sd
+        pick"sd*/sd" chmln/sd \
+        pick"jnv*/jnv" ynqa/jnv
 fi
