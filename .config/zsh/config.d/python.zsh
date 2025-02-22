da1553e27af386c51311771f3dedaaa0eb81ddc4
kevinzehnder <kevin.zehnder@protonmail.com>
2025-02-22 09:35:39 +0100

    use -qq for ftests

--- .config/zsh/config.d/python.zsh
+++ .config/zsh/config.d/python.zsh
@@ -1,5 +1,5 @@
 ftest () {
-  local tests=$(pytest --collect-only -q | rg '::' || echo "")
+  local tests=$(pytest --collect-only -qq | rg '::' || echo "")
   [[ -z "$tests" ]] && echo "No fucking tests found" && return 1
   local selected=$(echo "$tests" | fzf -m --height 40% --reverse)  || return 0
   local cmd="python -m pytest $(echo "$selected" | sed "s/.*/'&'/" | tr '\n' ' ') -v"
