a5d221164fa90f6c4cf9a528c1af545e0b2854ba
kevinzehnder <kevin.zehnder@protonmail.com>
2025-03-24 09:14:41 +0100

    fix more tools

--- .config/zsh/config.d/arm_tools.zsh
+++ .config/zsh/config.d/arm_tools.zsh
@@ -19,10 +19,12 @@ if [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
 # ARM 32-bit specific tools
 elif [[ "$ARCH" == "armv7l" ]]; then
 	zi wait lucid as"program" from"gh-r" for \
+		bpick"fd-v*-arm-unknown-linux-gnueabihf.tar.gz" mv"fd* -> fdfind" pick"fdfind/fd" atclone"sudo cp fdfind/fd /usr/bin/fd" @sharkdp/fd \
+		bpick"*-arm-unknown-linux-gnueabihf.tar.gz" mv"bat* -> bat" as"null" cp"bat/bat -> $ZPFX/bin/bat" @sharkdp/bat \
+		bpick"*arm-unknown-linux-gnueabihf.tar.gz" mv"dust* -> dust" as"null" cp"dust/dust -> $ZPFX/bin/dust" bootandy/dust \
 		bpick"eza_arm-unknown-linux-gnueabihf.tar.gz" eza-community/eza \
 		bpick"*armv7-unknown-linux-musleabihf*" denisidoro/navi \
 		bpick"*Linux_armv6*" jesseduffield/lazygit \
 		bpick"*armv7-unknown-linux-musleabi*" mv"ripgrep* -> rg" pick"rg/rg" BurntSushi/ripgrep \
 		bpick"*linux_armv7*" junegunn/fzf
-
 fi
