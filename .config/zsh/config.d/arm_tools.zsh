df712d71022584477e4a18bac4be920c9e0903ec
kevinzehnder <kevin.zehnder@protonmail.com>
2025-03-24 08:53:57 +0100

    add all arm versions

--- .config/zsh/config.d/arm_tools.zsh
+++ .config/zsh/config.d/arm_tools.zsh
@@ -1,25 +1,43 @@
-
-elif [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
-	# ARM 64-bit specific
+# ARM 64-bit specific tools
+if [[ "$ARCH" == "aarch64" || "$ARCH" == "arm64" ]]; then
 	zi wait lucid as"program" from"gh-r" for \
-		mv"ripgrep* -> rg" pick"rg/rg" BurntSushi/ripgrep \
+		bpick"*aarch64*" mv"dust* -> dust" pick"dust/dust" bootandy/dust \
+		bpick"*aarch64*" pick"duf" muesli/duf \
+		bpick"*aarch64*" mv"delta* -> delta" pick"delta/delta" dandavison/delta \
 		bpick"*aarch64*" eza-community/eza \
+		bpick"*aarch64*" mv"fd* -> fdfind" pick"fdfind/fd" atclone"sudo cp fdfind/fd /usr/bin/fd" @sharkdp/fd \
+		bpick"*aarch64*" mv"bat* -> bat" pick"bat/bat" @sharkdp/bat \
+		bpick"*aarch64*" atclone"sudo install procs /usr/bin/procs" dalance/procs \
+		bpick"*aarch64*" denisidoro/navi \
+		bpick"*aarch64*" pick"xh-*/xh" ducaale/xh \
+		bpick"*aarch64*" jesseduffield/lazygit \
+		bpick"*aarch64*" bensadeh/tailspin \
 		bpick"choose-aarch64-unknown-linux-gnu" binpicks"choose" mikefarah/choose \
+		bpick"*aarch64*" mv"ripgrep* -> rg" pick"rg/rg" BurntSushi/ripgrep \
 		bpick"*linux_arm64*" junegunn/fzf
 
-	zi ice as"program" id-as"neovim" \
-		atclone"CMAKE_BUILD_TYPE=Release make -j4" \
-		atpull"CMAKE_BUILD_TYPE=Release make -j4" \
-		pick"bin/nvim"
-	zi load neovim/neovim
-
+# ARM 32-bit specific tools
 elif [[ "$ARCH" == "armv7l" ]]; then
-	# ARM 32-bit specific
 	zi wait lucid as"program" from"gh-r" for \
-		mv"ripgrep* -> rg" pick"rg/rg" BurntSushi/ripgrep \
-		mv"choose* -> choose" bpick"choose-aarch64-unknown-linux-gnu" theryangeary/choose \
+		bpick"*armv7*" mv"dust* -> dust" pick"dust/dust" bootandy/dust \
+		bpick"*armv7*" pick"duf" muesli/duf \
+		bpick"*armv7*" mv"delta* -> delta" pick"delta/delta" dandavison/delta \
 		bpick"eza_arm-unknown-linux-gnueabihf.tar.gz" eza-community/eza \
+		bpick"*armv7*" mv"fd* -> fdfind" pick"fdfind/fd" atclone"sudo cp fdfind/fd /usr/bin/fd" @sharkdp/fd \
+		bpick"*armv7*" mv"bat* -> bat" pick"bat/bat" @sharkdp/bat \
+		bpick"*armv7*" atclone"sudo install procs /usr/bin/procs" dalance/procs \
+		bpick"*armv7*" denisidoro/navi \
+		bpick"*armv7*" pick"xh-*/xh" ducaale/xh \
+		bpick"*armv7*" jesseduffield/lazygit \
+		mv"choose* -> choose" bpick"choose-aarch64-unknown-linux-gnu" theryangeary/choose \
+		bpick"*armv7*" bensadeh/tailspin \
+		bpick"*armv7*" mv"ripgrep* -> rg" pick"rg/rg" BurntSushi/ripgrep \
 		bpick"*linux_armv7*" junegunn/fzf
 
-else
-	echo "Unknown architecture: $ARCH - some tools may not install correctly"
+	# Choose needs to be built from source on 32-bit ARM
+	zi ice lucid wait as"program" \
+		atclone"cargo build --release && cp target/release/choose $ZPFX/bin/" \
+		atpull"%atclone"
+	zi load theryangeary/choose
+
+fi
