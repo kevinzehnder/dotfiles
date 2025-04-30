32c8d7c044101fa017a673415a39acd0bab5bc95
kevinzehnder <kevin.zehnder@protonmail.com>
2025-04-30 09:40:24 +0200

    add python

---
+++ .config/zsh/config.d/python.zsh
@@ -0,0 +1,7 @@
+ftest() {
+	local tests=$(pytest --collect-only -qq | rg '::' || echo "")
+	[[ -z "$tests" ]] && echo "No fucking tests found" && return 1
+	local selected=$(echo "$tests" | fzf -m --height 40% --reverse) || return 0
+	local cmd="python -m pytest $(echo "$selected" | sed "s/.*/'&'/" | tr '\n' ' ') -v"
+	print -z "$cmd" # replace current line in shell with command
+}
