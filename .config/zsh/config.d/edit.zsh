fa35a4f3f249a7fd1548ee62316ec5fa3a14999f
kevinzehnder <kevin.zehnder@protonmail.com>
2025-06-16 01:10:48 +0200

    fix search functions

--- .config/zsh/config.d/edit.zsh
+++ .config/zsh/config.d/edit.zsh
@@ -1,20 +1,25 @@
-# Ripgrep thru files and edit them
 function fs() {
-	RG_BASE="rg --column --line-number --no-heading --color=always --smart-case --ignore-file-case-insensitive"
-	INITIAL_QUERY=""
+	local RG_BASE="rg --column --line-number --no-heading --color=always --smart-case --ignore-file-case-insensitive"
+	local INITIAL_QUERY=""
+	local HIDDEN_FLAG=""
+	# Create a temporary file to track hidden state
+	local hidden_state_file=$(mktemp)
+	echo "0" > "$hidden_state_file"
 	local match=$(
-		fzf --ansi --disabled --query "$INITIAL_QUERY" \
-			--bind "change:reload:$RG_BASE {q} 2>/dev/null || true" \
+		FZF_DEFAULT_COMMAND="$RG_BASE '$INITIAL_QUERY' 2>/dev/null || true" \
+			fzf --ansi --disabled --query "$INITIAL_QUERY" \
+			--bind "change:reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} 2>/dev/null || true" \
 			--bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+clear-query" \
-			--bind "ctrl-h:execute-silent([ -z $HIDDEN ] && export HIDDEN=1 || unset HIDDEN)+reload:$RG_BASE $([ -n $HIDDEN ] && echo '--hidden') {q} 2>/dev/null" \
+			--bind "ctrl-h:execute-silent([ \$(cat $hidden_state_file) -eq 0 ] && echo 1 > $hidden_state_file || echo 0 > $hidden_state_file)+reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} 2>/dev/null || true" \
 			--preview "bat --style=numbers --color=always {1} --highlight-line {2} 2>/dev/null" \
 			--preview-window=right:60%:wrap:+{2}-/2 \
 			--height=80% \
 			--prompt "1. ripgrep> " \
 			--delimiter : \
-			--header "CTRL-F: switch to fzf filtering | CTRL-H: toggle hidden files | ENTER: open in $EDITOR" \
-			< <(eval "$RG_BASE '$INITIAL_QUERY' 2>/dev/null")
+			--header "CTRL-F: switch to fzf filtering | CTRL-H: toggle hidden files | ENTER: open in $EDITOR"
 	)
+	# Clean up the temporary file
+	rm -f "$hidden_state_file"
 	local file=$(echo "$match" | cut -d':' -f1)
 	if [[ -n $file ]]; then
 		${EDITOR:-vim} "$file" "+$(echo "$match" | cut -d':' -f2)"
