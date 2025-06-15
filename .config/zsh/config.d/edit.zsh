9df4fe5333dd8b27b8563b280b0bdd5bb2ca2798
kevinzehnder <kevin.zehnder@protonmail.com>
2025-06-16 01:22:32 +0200

    improve fs previews

--- .config/zsh/config.d/edit.zsh
+++ .config/zsh/config.d/edit.zsh
@@ -1,4 +1,5 @@
 function fs() {
+	local search_dir="${1:-.}" # Use first argument or current directory
 	local RG_BASE="rg --column --line-number --no-heading --color=always --smart-case --ignore-file-case-insensitive"
 	local INITIAL_QUERY=""
 	local HIDDEN_FLAG=""
@@ -6,15 +7,15 @@ function fs() {
 	local hidden_state_file=$(mktemp)
 	echo "0" > "$hidden_state_file"
 	local match=$(
-		FZF_DEFAULT_COMMAND="$RG_BASE '$INITIAL_QUERY' 2>/dev/null || true" \
+		FZF_DEFAULT_COMMAND="$RG_BASE '$INITIAL_QUERY' '$search_dir' 2>/dev/null || true" \
 			fzf --ansi --disabled --query "$INITIAL_QUERY" \
-			--bind "change:reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} 2>/dev/null || true" \
+			--bind "change:reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} '$search_dir' 2>/dev/null || true" \
 			--bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+enable-search+clear-query" \
-			--bind "ctrl-h:execute-silent([ \$(cat $hidden_state_file) -eq 0 ] && echo 1 > $hidden_state_file || echo 0 > $hidden_state_file)+reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} 2>/dev/null || true" \
-			--preview "bat --style=numbers --color=always {1} --highlight-line {2} 2>/dev/null" \
+			--bind "ctrl-h:execute-silent([ \$(cat $hidden_state_file) -eq 0 ] && echo 1 > $hidden_state_file || echo 0 > $hidden_state_file)+reload:$RG_BASE \$([ \$(cat $hidden_state_file) -eq 1 ] && echo '--hidden') {q} '$search_dir' 2>/dev/null || true" \
+			--preview "BAT_STYLE=numbers bat --color=always --highlight-line {2} {1} 2>/dev/null" \
 			--preview-window=right:60%:wrap:+{2}-/2 \
 			--height=80% \
-			--prompt "1. ripgrep> " \
+			--prompt "1. ripgrep ($search_dir)> " \
 			--delimiter : \
 			--header "CTRL-F: switch to fzf filtering | CTRL-H: toggle hidden files | ENTER: open in $EDITOR"
 	)
